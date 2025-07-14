659750106feab4c6e6fd6a8d2903d341ea95d510
Sudhanshu Gupta <101333232+sudhanshugupta-git@users.noreply.github.com>
2025-07-14 10:33:20 +0530

    Update loggerMiddleware.js

--- Redux/todo_Redux_toolkit/src/redux/middlewares/loggerMiddleware.js
+++ Redux/todo_Redux_toolkit/src/redux/middlewares/loggerMiddleware.js
@@ -1,16 +1,28 @@
 // every middleware has access to store, next-middleware, action which is implemented in cascading manner using currying in js
 // steps: receive --> perform operation --> pass data to next middleware
 
+// recommended signature
+export const loggerMiddleware = store => next => action => {
+  console.log("[LOG]:", action.type, new Date().toString());
 
-export const loggerMiddleWare = (store)=>{
-    return function(next){
-        return function(action){
-            // log actions
-            console.log("[LOG]: "+action.type +" " +new Date().toString());
-            // call next middleware in the pipeline
-            next(action);
-            // log the modified state of app
-            console.log(store.getState())
-        }
-    }
-}
+  const result = next(action);
+
+  console.log(store.getState());
+
+  return result;
+};
+
+
+// export const loggerMiddleWare = (store)=>{
+//     return function(next){
+//         return function(action){
+//             // log actions
+//             console.log("[LOG]: "+action.type +" " +new Date().toString());
+//             // call next middleware in the pipeline
+//             next(action);
+//             // log the modified state of app
+//             console.log(store.getState())
+//         }
+//     }
+// }
+
